2a6c1b094847ab49b18f9f2a6219323fd3fdde33
BFronz <bfronz1960@me.com>
2020-05-19 16:57:56 -0400

    add bcrypt and jsonwebtokens

--- index.js
+++ index.js
@@ -22,19 +22,63 @@ app.use(cookieParser());
 // app.get('/', (req,res) => {res.send('hello world') });
 
 app.post("/api/users/register", (req, res) => {
-  var myData = new User(req.body);
-  console.log(myData);
-    myData.save()
-      .then(item => {
-          res.send("item saved to database");
+  var user = new User(req.body);
+   
+    user.save((err, doc) => {
+      if(err) return res.json({success: false, err});
+      res.status(200).json({
+        success: true,
+        userData: doc
+      })
+    })
+ })
+
+// app.post("/api/users/register", (req, res) => {
+//   var myData = new User(req.body);
+//     console.log(myData);
+
+
+//     myData.save()
+//       .then(item => {
+//           res.send("item saved to database");
+//         })
+//       .catch(err => {
+//           res.status(400).send("unable to save to database");
+//        });
+//  });
+
+
+app.post('/api/usr/login', (req, res) =>{
+  // find the email
+  User.findOne({email: req.body.email}, (err, user) =>{
+    if(user)
+    return res.json({
+      loginSuccess: false,
+      message: "Auth failed, email not found"
+    })
+
+    // compare pw   
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) {
+        return res.json ({ loginSuccess: false, message: "wrong password"})
+      }
+    })
+    // generate token
+    user.generateToken(( err, user) => {
+      if(err) return res.status (400).send(err);
+      res.cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true
         })
-      .catch(err => {
-          res.status(400).send("unable to save to database");
-       });
- });
+    })
+  })
+
+  
 
 
-
+  
+})
   
 
 
