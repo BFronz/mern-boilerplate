dc0f9b0e49bccc1498687eb7873804b1ffcac4c9
BFronz <bfronz1960@me.com>
2020-05-19 14:44:30 -0400

    add hidden keys devjs file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const bodyParser =  require('body-parser');
 const cookieParser =  require('cookie-parser');
 const { User } = require('./models/user');
 
-mongoose.connect('mongodb+srv://dbMernUser:Mern2020@mern-boilerplate-gd7xv.mongodb.net/test?retryWrites=true&w=majority', 
-{ useNewUrlParser: true } )
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI, 
+{ useNewUrlParser: true , useUnifiedTopology: true  } )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
@@ -17,24 +19,23 @@ app.use(bodyParser.urlencoded({ extended : true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// app.get('/', (req,res) => {res.send('hello world') });
+
+app.post("/api/users/register", (req, res) => {
+  var myData = new User(req.body);
+  console.log(myData);
+    myData.save()
+      .then(item => {
+          res.send("item saved to database");
+        })
+      .catch(err => {
+          res.status(400).send("unable to save to database");
+       });
+ });
 
 
-// app.get('/', (req,res) => {
-//    res.send('hello world') 
-// });
-
-app.post('/api/users/register',  (req, res)  => {
-      const user =  new User(req.body);
-     
-      user.save( (err, userData) => {
-        if(err) return res.json({sucess: false})
-
-      });
-      return res.status(200)
-  })
 
   
-
 
 
 // Start the API server
--- models/user.js
+++ models/user.js
@@ -1,36 +1,29 @@
 const mongoose = require('mongoose');
 
-const userSchema = moongoose.Schema({
-
-    name:{
+const userSchema = mongoose.Schema({
+    name: {
         type: String,
         maxlength: 50
     },
     email: {
         type: String,
         trim: true,
-        unique: 1
+        unique: true,
     },
     password: {
         type: String,
-        minlength:5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
+        minglength: 5
     },
     role: {
         type: Number,
         default: 0
     },
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    }
-
-});
+    image: String,
+    token: String,
+    tokenExp: Number,
+    resetToken: String,
+    resetTokenExp: Number
+})
 
 const User = mongoose.model('User', userSchema);
 
