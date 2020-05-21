61d4d1db8eb1b800397ba3c46fbcc562d45532b1
BFronz <bfronz1960@me.com>
2020-05-21 13:22:11 -0400

    add some styles

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default class About extends Component {
     render() {
         return (
             <div>
-                This is the about page
+                <div className="containter">
+                
+                <div>
+                    <h5 class="center-align">MERN Boilerplate</h5>
+                    <h6 class="center-align">About Page</h6>
+                </div>
+
+
+                <div class="card-panel hoverable center-align">
+ 
+                <div className="row">
+                    <div className="col s12">
+        
+                    <Link to="/register">
+                    <button className="btn waves-effect red loghten"
+                    type="link"
+                    name ="action"
+                    >
+                    Sign Up
+                    </button>     
+                 </Link> 
+
+                &nbsp;&nbsp;&nbsp;
+                 <Link to="/about">
+                <button className="btn waves-effect red loghten"
+                    type="submit"
+                     name ="action"
+                     >
+                    About
+                    </button>     
+                    </Link> 
+                     &nbsp;&nbsp;&nbsp;
+                    <Link to="/login">
+                    <button className="btn waves-effect red loghten"
+                    type="link"
+                     name ="action"
+                    >
+                    Login
+                 </button>      
+                </Link> 
+
+                &nbsp;&nbsp;&nbsp;
+                 <Link to="/">
+                <button className="btn waves-effect red loghten"
+                    type="link"
+                     name ="action"
+                     >
+                    Home
+                    </button>     
+                    </Link> 
+                   
+
+
+
+        </div>
+    </div>
+
+
+</div>
+
+</div>
+
             </div>
         );
     }
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
         return (
+            
             <div>
-                This is the home page
+            <div className="containter">
+            
+            <div>
+                <h5 class="center-align">MERN Boilerplate</h5>
+                <h6 class="center-align">Home Page</h6>
             </div>
+
+
+            <div class="card-panel hoverable center-align">
+
+            <div className="row">
+                <div className="col s12">
+    
+                <Link to="/register">
+                <button className="btn waves-effect red loghten"
+                type="link"
+                name ="action"
+                >
+                Sign Up
+                </button>     
+             </Link> 
+
+            &nbsp;&nbsp;&nbsp;
+             <Link to="/about">
+            <button className="btn waves-effect red loghten"
+                type="submit"
+                 name ="action"
+                 >
+                About
+                </button>     
+                </Link> 
+                 &nbsp;&nbsp;&nbsp;
+                <Link to="/login">
+                <button className="btn waves-effect red loghten"
+                type="link"
+                 name ="action"
+                >
+                Login
+             </button>      
+            </Link> 
+
+            &nbsp;&nbsp;&nbsp;
+             <Link to="/">
+            <button className="btn waves-effect red loghten"
+                type="link"
+                 name ="action"
+                 >
+                Home
+                </button>     
+                </Link> 
+               
+
+
+
+    </div>
+</div>
+
+
+</div>
+
+</div>
+
+        </div>
         );
     }
 }
